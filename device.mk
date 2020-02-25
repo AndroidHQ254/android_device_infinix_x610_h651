4912b8cf1d3acdb1237bf790f451a309317eb128
Kshitij Gupta <kshitijgm@gmail.com>
2020-02-25 20:49:07 +0000

    x610_h651: Use a custom Lights HAL
    - Based on Lights from violet
    - See: https://github.com/PotatoDevices/device_xiaomi_violet/tree/croquette-release/light

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.x610_h651
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_mediatek_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_audio.xml \
