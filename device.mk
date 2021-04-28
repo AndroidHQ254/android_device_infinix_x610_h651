40d33a26bfb161a505afb85934a5b21916438374
Erfan Abdi <erfangplus@gmail.com>
2021-04-28 03:54:57 +0200

    x610_h651: Cleanup rootdir scripts

--- device.mk
+++ device.mk
@@ -161,23 +161,13 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    factory_init.connectivity.rc \
-    factory_init.project.rc \
-    factory_init.rc \
     fstab.enableswap \
     fstab.mt6765 \
-    init.ago.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6765.rc \
     init.mt6765.usb.rc \
-    init.project.rc \
     init.sensor_1_0.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    multi_init.rc \
     partition_permission.sh \
     throttle.sh \
     ueventd.mt6765.rc
