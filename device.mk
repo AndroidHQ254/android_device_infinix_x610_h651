75ad1605592d7cc5aeb24336d822f37154c7a96b
danascape <danascape@gmail.com>
2021-09-01 10:46:15 +0530

    x610_h651: Build RenderScript HAL

--- device.mk
+++ device.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
     throttle.sh \
     ueventd.mt6765.rc
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
