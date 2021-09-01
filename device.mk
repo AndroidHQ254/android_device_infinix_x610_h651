8f3ccaba490d698059534fe35e85afe0b4f79cf9
danascape <danascape@gmail.com>
2021-09-01 10:46:15 +0530

    breeze: Build RenderScript HAL
    Change-Id: I75e93dde72aadc7680e220a100d3fb015b326f73

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     partition_permission.sh \
     throttle.sh
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
