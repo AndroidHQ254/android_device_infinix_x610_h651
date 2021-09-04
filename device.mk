40b11802b5f76592af032c69f30706fe4464cbbe
Debayan Kar <kardebayan3@gmail.com>
2021-09-04 23:14:15 +0530

    x610_h651: Inherit proprietary blobs
    * Also track our vendor repo while we are at it

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/infinix/x610_h651/x610_h651-vendor.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0 \
