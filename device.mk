b19b42fee152c80a09d2035d51731e5ac3d1a818
TheMalachite <eliasgheeraert@gmail.com>
2021-10-11 10:25:58 +0530

    x610_h651: Build Display Hals from source

--- device.mk
+++ device.mk
@@ -8,6 +8,16 @@
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.common@1.2 \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    libhwc2onfbadapter
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
