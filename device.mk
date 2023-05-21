a838a7155b5611495f8d96cc2e4e6dfda7b2ed18
AndroidHQ254 <d.murimi14@gmail.com>
2023-05-21 22:31:41 +0300

    clean up

--- device.mk
+++ device.mk
@@ -19,24 +19,15 @@ PRODUCT_CHARACTERISTICS := default
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6765 \
-    meta_init.project.rc \
     init.mt6765.usb.rc \
-    meta_init.connectivity.rc \
     init.sensor_1_0.rc \
-    meta_init.modem.rc \
-    init.ago.rc \
     factory_init.rc \
     init.modem.rc \
-    factory_init.project.rc \
     init.mt6765.rc \
     meta_init.rc \
-    factory_init.connectivity.rc \
-    multi_init.rc \
     init.connectivity.rc \
-    init.project.rc \
     init.aee.rc \
     ueventd.rc \
-    init.rc \
     init.recovery.mt6765.rc \
 
 # Shipping API level
