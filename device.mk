00d55745a0b8488581ac34f8a98fce16159e32c3
AndroidHQ254 <d.murimi14@gmail.com>
2023-05-30 16:41:48 +0300

    x610_h651: Downrev android.hardware.wifi to 1.0
    W ServiceManagement: Waited one second for android.hardware.wifi@1.0::IWifi/default
    I ServiceManagement: getService: Trying again for android.hardware.wifi@1.0::IWifi/default...

--- device.mk
+++ device.mk
@@ -306,9 +306,9 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 28
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.2.vendor \
+    android.hardware.wifi@1.0.vendor \
     android.hardware.wifi.supplicant@1.1.vendor \
-    android.hardware.wifi.hostapd@1.3.vendor
+    android.hardware.wifi.hostapd@1.1.vendor
 
 PRODUCT_PACKAGES += \
     libkeystore-engine-wifi-hidl \
