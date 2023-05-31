d7c628e91d0244feb90ae387fa7c4d502182a860
AndroidHQ254 <d.murimi14@gmail.com>
2023-06-01 01:09:45 +0300

    x610_h651: Initial tree skeleton

---
+++ lineage_x610_h651.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from x610_h651 device
+$(call inherit-product, device/infinix/x610_h651/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_x610_h651
+PRODUCT_DEVICE := x610_h651
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := Infinix Note 6
+PRODUCT_MANUFACTURER := Infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_x610_h651-user 9 PPR1.180610.011 31055 release-keys"
+
+BUILD_FINGERPRINT := Infinix/H651B/Infinix-X610:9/PPR1.180610.011/AB-200805V370:user/release-keys
