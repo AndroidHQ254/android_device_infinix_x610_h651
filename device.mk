d4b3d7f49a1d397e4ffc03c7722707611c5ccaa4
AndroidHQ254 <d.murimi14@gmail.com>
2023-05-31 03:57:05 +0300

    x610_h651: Update keylayout configs from stock

--- device.mk
+++ device.mk
@@ -136,6 +136,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ACCDET.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/ACCDET.kl \
     $(LOCAL_PATH)/keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl
 
 # Keymaster
