e0602b78cbb1956cd0335b2b1c357a4565c09ed3
AndroidHQ254 <d.murimi14@gmail.com>
2023-07-01 16:52:16 +0300

    x610_h651: Some refactoring

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
-    tinymix \
     libxml2.vendor
 
 PRODUCT_COPY_FILES += \
